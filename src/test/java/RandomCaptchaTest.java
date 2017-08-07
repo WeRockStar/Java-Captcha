fd5e1b80c844da32f6135e525a27024b47de6274
WeRockStar <kotchaphan.m@gmail.com>
2017-08-08 01:10:23 +0700

    Add spy random

--- src/test/java/RandomCaptchaTest.java
+++ src/test/java/RandomCaptchaTest.java
@@ -13,11 +13,36 @@ public class RandomCaptchaTest {
         assertEquals(1, randomCaptcha.getPattern());
     }
 
+    @Test
+    public void verify_random_next_int_should_call_once() throws Exception {
+        // Arrange
+        SpyRandom random = new SpyRandom();
+        RandomCaptcha randomCaptcha = new RandomCaptcha(random);
+
+        // Act
+        randomCaptcha.getPattern();
+
+        // Assert
+        assertTrue(random.verifyNextIntHaveBeenCalledOnce());
+    }
+
     private class SpyRandom extends Random {
+        private int bound;
+        private int count = 0;
 
         @Override
         public int nextInt(int bound) {
+            this.bound = bound;
+            this.count++;
             return 1;
+        }
+
+        public int getBound() {
+            return bound;
+        }
+
+        public boolean verifyNextIntHaveBeenCalledOnce() {
+            return count == 1;
         }
     }
 }
