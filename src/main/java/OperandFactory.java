a725dba69607c10d1d674da1da86ac153c45d121
WeRockStar <kotchaphan.m@gmail.com>
2017-12-11 21:52:57 +0700

    Replace if statement with command pattern

--- src/main/java/OperandFactory.java
+++ src/main/java/OperandFactory.java
@@ -1,11 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class OperandFactory {
 
     public static Operand createLeft(int pattern, int operand) {
-        return pattern == 1 ? new StringOperand(operand) : new IntegerOperand(operand);
+        Map<Integer, Operand> patterns = new HashMap<>();
+        patterns.put(1, new StringOperand(operand));
+        patterns.put(2, new IntegerOperand(operand));
+        return patterns.get(pattern);
     }
 
     public static Operand createRight(int pattern, int operand) {
-        return pattern == 1 ? new IntegerOperand(operand) : new StringOperand(operand);
+        Map<Integer, Operand> patterns = new HashMap<>();
+        patterns.put(1, new IntegerOperand(operand));
+        patterns.put(2, new StringOperand(operand));
+        return patterns.get(pattern);
     }
 }
